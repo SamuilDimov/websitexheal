953f2f0a326611ec3e16feceff5cd13a1b528f1d
trifon-getsov <trifon@getsov.pro>
2026-02-20 13:47:16 +0200

    Use light blue from design system for 'Free to download' text

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -81,7 +81,7 @@ export default function HeroSection() {
                   priority
                 />
               </a>
-              <span className="text-[1rem] text-xwhite">
+              <span className="text-[1rem] text-xlight-blue">
                 Free to download. Your data stays yours.
               </span>
             </div>
